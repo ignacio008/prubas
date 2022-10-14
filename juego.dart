a33f0aa8ea9d9012c33acd92c6b0361bb392ad86
ignacio <ignacioalex008.com>
2022-10-13 19:26:52 -0500

    version prueb

---
+++ juego.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class Yolo extends StatefulWidget {
+  const Yolo({super.key});
+
+  @override
+  State<Yolo> createState() => _YoloState();
+}
+
+class _YoloState extends State<Yolo> {
+  String yolo="";
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
